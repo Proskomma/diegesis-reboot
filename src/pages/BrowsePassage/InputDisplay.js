c4d5078f73e55e9f599686f734469bb9f961bf37
eliaspinero <eliasrpt14@gmail.com>
2022-03-24 13:09:21 -0430

    Added colors and a new condition to InputDisplay. #55

--- src/pages/BrowsePassage/InputDisplay.js
+++ src/pages/BrowsePassage/InputDisplay.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {IonIcon} from '@ionic/react';
-import { checkmarkCircleOutline, closeCircle } from 'ionicons/icons';
+import { checkmarkCircle, closeCircle } from 'ionicons/icons';
 
 export default function InputDisplay({parseR}) {
-    return parseR.parsed && parseR.startVerse ? <IonIcon size="large" icon={checkmarkCircleOutline} /> : <IonIcon size="large" icon={closeCircle} />
+    return Object.keys(parseR).length === 0 || parseR.parsed && parseR.startVerse ? 
+    <IonIcon 
+    size="large" 
+    color="success" 
+    icon={checkmarkCircle} 
+    /> : 
+    <IonIcon 
+    size="large" 
+    color="danger" 
+    icon={closeCircle} 
+    />
 }
 
 InputDisplay.propTypes = {
