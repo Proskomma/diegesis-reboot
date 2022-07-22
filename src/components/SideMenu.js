c189d37e3d1544a49a6968251f3e2a2cc80999be
gabriel <aillet.gabriel05@gmail.com>
2022-07-22 10:39:21 +0200

    search and download from server

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,52 +1,106 @@
-import {IonContent, IonHeader, IonMenu, IonToolbar, IonButtons, IonButton, IonIcon} from "@ionic/react";
-import React, {useState} from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+} from "@ionic/react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import SideMenuNavigation from "./SideMenuNavigation";
-import SideMenuSearch from "./SideMenuSearch";
-import {albums, albumsOutline, search, searchOutline, settings} from 'ionicons/icons';
+import NSideMenuSearch from "./NSideMenuSearch";
+
+import {
+  albums,
+  albumsOutline,
+  search,
+  searchOutline,
+  settings,
+  download,
+  downloadOutline,
+} from "ionicons/icons";
 import AppLangOptions from "./AppLangOptions";
+import SideMenuBibleDownload from "./SideMenuBibleDownload";
 
-export default function SideMenu({catalog, pkState, navState, setNavState, appLanguage, setAppLanguage}) {
 
-    const [selected, setSelected] = useState('navigation');
-    const [showAppLang, setShowAppLang] = useState(false);
+export default function SideMenu({
+  catalog,
+  pkState,
+  navState,
+  setNavState,
+  appLanguage,
+  setAppLanguage,
+}) {
 
-    return <IonMenu
-        side="start"
-        type="overlay"
-        contentId="main"
-        swipeGesture={true}
-    >
-        <IonHeader>
-            <IonToolbar color="secondary">
-                <IonButtons slot="start">
-                    <IonButton fill="clear" onClick={() => setSelected('navigation')}>
-                        <IonIcon icon={selected === 'navigation' ? albums : albumsOutline} />
-                    </IonButton>
-                    <IonButton fill="clear" onClick={() => setSelected('search')}>
-                        <IonIcon icon={selected === 'search' ? search : searchOutline} />
-                    </IonButton>
-                </IonButtons>
-                <IonButtons slot="end">
-                    <IonButton size="small" fill="clear" onClick={() => setShowAppLang(!showAppLang)}>
-                        <IonIcon icon={settings} />
-                    </IonButton>
-                </IonButtons>
-            </IonToolbar>
-        </IonHeader>
-        <IonContent>
-            {showAppLang && <AppLangOptions appLanguage={appLanguage} setAppLanguage={setAppLanguage} />}
-            {selected === 'navigation' && <SideMenuNavigation catalog={catalog} navState={navState} setNavState={setNavState} />}
-            {selected === 'search' && <SideMenuSearch pkState={pkState} navState={navState} setNavState={setNavState} />}
-        </IonContent>
+  const [selected, setSelected] = useState("navigation");
+  const [showAppLang, setShowAppLang] = useState(false);
+
+  return (
+    <IonMenu side="start" type="overlay" contentId="main" swipeGesture={true}>
+      <IonHeader>
+        <IonToolbar color="secondary">
+          <IonButtons slot="start">
+            <IonButton fill="clear" onClick={() => setSelected("navigation")}>
+              <IonIcon
+                icon={selected === "navigation" ? albums : albumsOutline}
+              />
+            </IonButton>
+            <IonButton fill="clear" onClick={() => setSelected("search")}>
+              <IonIcon icon={selected === "search" ? search : searchOutline} />
+            </IonButton>
+            <IonButton fill="clear" onClick={() => setSelected("download")}>
+              <IonIcon
+                icon={selected === "download" ? download : downloadOutline}
+              />
+            </IonButton>
+          </IonButtons>
+          <IonButtons slot="end">
+            <IonButton
+              size="small"
+              fill="clear"
+              onClick={() => setShowAppLang(!showAppLang)}
+            >
+              <IonIcon icon={settings} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {showAppLang && (
+          <AppLangOptions
+            appLanguage={appLanguage}
+            setAppLanguage={setAppLanguage}
+          />
+        )}
+        {selected === "navigation" && (
+          <SideMenuNavigation
+            navState={navState}
+            setNavState={setNavState}
+            catalog={catalog}
+          />
+        )}
+        {selected === "search" && (
+          <NSideMenuSearch
+            pkState={pkState}
+            navState={navState}
+            setNavState={setNavState}
+          />
+        )}
+        {selected === "download" && (
+          <SideMenuBibleDownload pkState={pkState} catalog={catalog} />
+        )}
+      </IonContent>
     </IonMenu>
+  );
 }
 
 SideMenu.propTypes = {
-    catalog: PropTypes.object.isRequired,
-    pkState: PropTypes.object.isRequired,
-    navState: PropTypes.object.isRequired,
-    setNavState: PropTypes.func.isRequired,
-    appLanguage: PropTypes.string.isRequired,
-    setAppLanguage: PropTypes.func.isRequired,
+  catalog: PropTypes.object.isRequired,
+  pkState: PropTypes.object.isRequired,
+  navState: PropTypes.object.isRequired,
+  setNavState: PropTypes.func.isRequired,
+  appLanguage: PropTypes.string.isRequired,
+  setAppLanguage: PropTypes.func.isRequired,
 };
